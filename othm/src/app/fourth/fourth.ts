b2b595a84459cf7d2800c3aca8de6631de4d2dab
o-a <othman.ali@iisgalvanimi.edu.it>
2026-01-15 07:59:38 +0000

    punto 2

---
+++ othm/src/app/fourth/fourth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth',
+  imports: [],
+  templateUrl: './fourth.html',
+  styleUrl: './fourth.css',
+})
+export class Fourth {
+
+}
